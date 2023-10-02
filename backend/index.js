17e940c081433091380dfbe2c65e6c0456c42e7b
Archit Sehgal <architsehgal19@gmail.com>
2023-10-02 14:50:56 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,6 @@ app.get("/", async (req, res) => {
 
     try {
         const response = await axios.request(options);
-        console.log(response.data);
         res.json(response.data);
     } catch (error) {
         console.error(error);
