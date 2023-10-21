e8db6d9a8d097da30f483abaa69b9a819110a68d
Archit Sehgal <architsehgal19@gmail.com>
2023-10-21 12:32:24 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,114 +1,117 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const app = express();
 const axios = require("axios");
-require('dotenv').config(); 
+require('dotenv').config();
 
 const port = process.env.PORT;
-const rapidapi=process.env.rapidapi
-const RapidHost=process.env.RapidHost
-const newsurl=process.env.newsurl
+const rapidapi = process.env.rapidapi;
+const RapidHost = process.env.RapidHost;
+const newsurl = process.env.newsurl;
 
 app.use(cors());
 
-app.get("/", async (req, res) => {
-    const symbol = req.query.symbolName;
-    const options = {
-        method: 'GET',
-        url: 'https://latest-stock-price.p.rapidapi.com/price',
-        params: {
-            Indices: 'NIFTY 500',
-            Identifier: symbol
-        },
-        headers: {
-            'X-RapidAPI-Key': rapidapi,
-            'X-RapidAPI-Host': RapidHost
-        }
-    };
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+});
 
-    try {
-        const response = await axios.request(options);
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
+app.use("/api/", apiLimiter);
+
+app.get("/", async (req, res) => {
+  const symbol = req.query.symbolName;
+  const options = {
+    method: 'GET',
+    url: 'https://latest-stock-price.p.rapidapi.com/price',
+    params: {
+      Indices: 'NIFTY 500',
+      Identifier: symbol
+    },
+    headers: {
+      'X-RapidAPI-Key': rapidapi,
+      'X-RapidAPI-Host': RapidHost
     }
+  };
+
+  try {
+    const response = await axios.request(options);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.get("/help", async (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://latest-stock-price.p.rapidapi.com/price',
-        params: {
-            Indices: 'NIFTY 500'
-        },
-        headers: {
-            'X-RapidAPI-Key': rapidapi,
-            'X-RapidAPI-Host': RapidHost
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        const stockData = response.data.map((stock) => ({
-            name: stock.symbol,
-            identifier: stock.identifier,
-        }));
-        res.json(stockData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+  const options = {
+    method: 'GET',
+    url: 'https://latest-stock-price.p.rapidapi.com/price',
+    params: {
+      Indices: 'NIFTY 500'
+    },
+    headers: {
+      'X-RapidAPI-Key': rapidapi,
+      'X-RapidAPI-Host': RapidHost
     }
+  };
+
+  try {
+    const response = await axios.request(options);
+    const stockData = response.data.map((stock) => ({
+      name: stock.symbol,
+      identifier: stock.identifier,
+    }));
+    res.json(stockData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-
-// single stock details
 app.get("/ss", async (req, res) => {
-    const symbol = req.query.identifier;
+  const symbol = req.query.identifier;
 
-    const options = {
-        method: 'GET',
-        url: 'https://latest-stock-price.p.rapidapi.com/price',
-        params: {
-            Indices: 'NIFTY 500',
-            Identifier: symbol
-        },
-        headers: {
-            'X-RapidAPI-Key': rapidapi,
-            'X-RapidAPI-Host': RapidHost
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
+  const options = {
+    method: 'GET',
+    url: 'https://latest-stock-price.p.rapidapi.com/price',
+    params: {
+      Indices: 'NIFTY 500',
+      Identifier: symbol
+    },
+    headers: {
+      'X-RapidAPI-Key': rapidapi,
+      'X-RapidAPI-Host': RapidHost
     }
+  };
+
+  try {
+    const response = await axios.request(options);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-
-// news section
 app.get("/topten", async (req, res) => {
-    try {
-        const response = await axios.get(newsurl);
-        const articles = response.data.articles;
+  try {
+    const response = await axios.get(newsurl);
+    const articles = response.data.articles;
 
-        const newsData = articles.map((article) => ({
-            newsName: article.source.name,
-            newsTitle: article.title,
-            newsDesc: article.description,
-            newsUrl: article.url,
-        }));
+    const newsData = articles.map((article) => ({
+      newsName: article.source.name,
+      newsTitle: article.title,
+      newsDesc: article.description,
+      newsUrl: article.url,
+    }));
 
-        res.status(200).json(newsData);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    res.status(200).json(newsData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
-
 
 app.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
-  });
-  
+  console.log(`Server started on port: ${port}`);
+});
